f3abfe6a5b5c7f1d242c610637bb4fc52bbe9418
Het Savani <114601993+Hetsavani@users.noreply.github.com>
2024-08-18 19:13:28 +0530

    Update StoredChat.js

--- src/StoredChat.js
+++ src/StoredChat.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 function StoredChat() {
   const nav = useNavigate();
   const name = sessionStorage.getItem("username");
-  const apiUrl = "http://localhost:3030/getstoredmeetings";
+  const apiUrl = "https://chatapi-sgoo.onrender.com/getstoredmeetings";
   const userId = sessionStorage.getItem("userID");
   const [meetings, setMeetings] = useState([]);
   const [temp,settemp] = useState(false);
